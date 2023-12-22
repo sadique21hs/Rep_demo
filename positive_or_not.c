8cd8da089e6fc8282fcd9fc8fa8126f5b9d299e6
Md Sadique Hossain <152019380+sadique21hs@users.noreply.github.com>
2023-12-23 01:16:03 +0600

    Update positive_or_not.c

--- positive_or_not.c
+++ positive_or_not.c
@@ -7,12 +7,16 @@ int main()
     printf("Enter the number A: ");  
     scanf("%d", &A);  
   
-    if (A > 0)  
-        printf("%d is positive.", A);  
-    else if (A < 0)  
-        printf("%d is negative.", A);  
-    else if (A == 0)  
-        printf("%d is zero.", A);  
+    if (A > 0){  
+                  printf("%d is positive.", A); 
+              } 
+    else if (A < 0) {
+              
+                  printf("%d is negative.", A);
+                   } 
+    else if (A == 0) { 
+                    printf("%d is zero.", A); 
+                    }
   
     return 0;  
 }  
